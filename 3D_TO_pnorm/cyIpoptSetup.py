1a32009fa300ece899d16d69ab058044ea475057
sashalsey <sasha.halsey20@imperial.ac.uk>
2025-01-17 14:58:42 +0000

    Added stress constraint files

---
+++ 3D_TO_pnorm/cyIpoptSetup.py
@@ -0,0 +1,57 @@
+import numpy as np # type: ignore
+import cyipopt # type: ignore
+
+def CyIpoptWrapper(optimisationClass):
+    numberOfVariables = len(optimisationClass.djdrho)
+    numberOfConstraints = len(optimisationClass.c)
+
+    # design variable bounds
+    lowerBounds = np.ones(numberOfVariables) * optimisationClass.lowerBound
+    upperBounds = np.ones(numberOfVariables) * optimisationClass.upperBound
+
+    # constraint bounds
+    # constraint 1 - volume fraction, 2 - stress
+    constraintLowerBounds = np.array([0, 0])
+    constraintUpperBounds = np.array([0, 0])
+
+    # cyipopt class
+    class OptimisationSetup:
+        def __init__(self):
+            pass
+
+        def objective(self, designVariables):
+            return optimisationClass.Objective(designVariables)
+
+        def gradient(self, designVariables):
+            return optimisationClass.Gradient(designVariables)
+
+        def constraints(self, designVariables):
+            return optimisationClass.Constraints(designVariables)
+
+        def jacobian(self, designVariables):
+            return optimisationClass.Jacobian(designVariables)
+
+    # cyipopt setup
+    optimisationProblem = cyipopt.Problem(
+        n=numberOfVariables,
+        m=numberOfConstraints,
+        problem_obj=OptimisationSetup(),
+        lb=lowerBounds,
+        ub=upperBounds,
+        cl=constraintLowerBounds,
+        cu=constraintUpperBounds,)
+
+    optimisationProblem.add_option("linear_solver", "ma97")
+    optimisationProblem.add_option("max_iter", optimisationClass.maximumNumberOfIterations)
+    optimisationProblem.add_option("accept_after_max_steps", 10)
+    optimisationProblem.add_option("hessian_approximation", "limited-memory")
+    optimisationProblem.add_option("mu_strategy", "adaptive")
+    optimisationProblem.add_option("mu_oracle", "probing")
+    optimisationProblem.add_option("tol", 1e-5)
+    optimisationProblem.set_problem_scaling(obj_scaling=optimisationClass.gradientScaling)
+    optimisationProblem.add_option("nlp_scaling_method", "user-scaling")
+
+    # Solve the problem
+    xOptimal, info = optimisationProblem.solve(optimisationClass.designVariables0)
+
+    return xOptimal
