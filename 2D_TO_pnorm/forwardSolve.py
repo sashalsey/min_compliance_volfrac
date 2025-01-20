b1abece390520ec21599787b7b87b258c8f55b2e
sashalsey <sasha.halsey20@imperial.ac.uk>
2025-01-20 16:07:19 +0000

    P-norm stress in 2D, well, I tried

---
+++ 2D_TO_pnorm/forwardSolve.py
@@ -0,0 +1,226 @@
+import numpy as np  # type: ignore
+import firedrake as fd  # type: ignore
+import firedrake.adjoint as fda  # type: ignore
+from firedrake.output import VTKFile # type: ignore
+fda.continue_annotation()
+
+class ForwardSolve:
+    def __init__(self, outputFolder, outputFolder2, beta, penalisationExponent, variableInitialisation, rho0, pnorm):
+        # append inputs to class
+        self.outputFolder = outputFolder
+        self.outputFolder2 = outputFolder2
+
+        # rho initialisation
+        self.variableInitialisation = variableInitialisation
+        self.penalisationExponent = penalisationExponent
+        self.rho0 = rho0
+
+        # material properties
+        self.E0 = 1e-6  # [Pa] # void
+        self.E1 = 2.7e9  # [Pa] # dense
+        self.nu = 0.3  # []
+
+        # pseudo-density functional
+        self.beta = beta  # heaviside projection parameter
+        self.eta0 = 0.5  # midpoint of projection filter
+
+        self.Vlimit = 0.3
+        self.Slimit = 20e6
+        self.pnorm = pnorm
+
+    def GenerateMesh(self,):
+        self.mesh = fd.Mesh('corner.msh')
+        # self.mesh = fd.RectangleMesh(self.nx, self.ny, self.lx, self.ly, quadrilateral=True)
+        self.gradientScale = (self.nx * self.ny) / (self.lx * self.ly)  # firedrake bug?
+
+    def Setup(self):
+        # mesh, functionals and associated static parameters
+        self.nx, self.ny = 15, 15
+        self.lx, self.ly = 0.5, 0.5
+        self.GenerateMesh()
+
+        # compute mesh volume
+        self.meshVolume = fd.assemble(1 * fd.dx(domain=self.mesh))
+
+        # define function spaces
+        self.functionSpace = fd.FunctionSpace(self.mesh, "DG", 0)
+        self.vectorFunctionSpace = fd.VectorFunctionSpace(self.mesh, "CG", 2)
+
+        # define psuedo-density function
+        self.rho = fd.Function(self.functionSpace)
+
+        # number of nodes in mesh
+        self.numberOfNodes = len(self.rho.vector().get_local())
+
+        # compute helmholtz filter radius
+        self.helmholtzFilterRadius = 1 * (self.meshVolume / self.mesh.num_cells()) ** (1 / self.mesh.cell_dimension())
+
+        # boundary conditions, add ne  by adding new key to dict
+        bcDict = {}
+        bcDict[0] = fd.DirichletBC(self.vectorFunctionSpace,fd.Constant((0, 0)), 7)
+        self.bcs = [bcDict[i] for i in range(len(bcDict.keys()))]
+
+        # define output files
+        self.rho_hatFile = VTKFile(self.outputFolder + "rho_hat.pvd")
+        self.rho_hatFunction =fd.Function(self.functionSpace, name="rho_hat")
+
+        self.uFile = VTKFile(self.outputFolder + "u.pvd")
+        self.uFunction =fd.Function(self.vectorFunctionSpace, name="u")
+
+        self.stressFile = VTKFile(self.outputFolder + "stress.pvd")
+        self.stressFunction = fd.Function(self.functionSpace, name="stress")
+
+    def ComputeInitialSolution(self):
+
+        if self.variableInitialisation is True:
+            initialisationFunction =fd.Function(self.functionSpace)
+            initialisationFunction.vector().set_local(self.rho0)
+        else:
+
+            # initialise function for initial solution
+            initialisationFunction =fd.Function(self.functionSpace)
+
+            # generate uniform initialisation
+            initialisationFunction.assign(self.Vlimit)
+
+        return initialisationFunction.vector().get_local()
+
+    def CacheDesignVariables(self, designVariables, initialise=False):
+        if initialise is True:
+            # initialise with zero length array
+            self.rho_np_previous = np.zeros(0)
+            cache = False
+        else:
+            # determine whether the current design variables were simulated at previous iteration
+            if np.array_equal(designVariables, self.rho_np_previous):
+
+                # update cache boolean
+                cache = True
+
+            else:
+                # new array is unique
+                # assign current array to cache
+                self.rho_np_previous = designVariables
+
+                # update self.rho
+                self.rho.vector().set_local(designVariables)
+
+                # update cache boolean
+                cache = False
+
+        return cache
+
+    def Solve(self, designVariables):
+        # insert and cache design variables
+        # automatically updates self.rho
+        identicalVariables = self.CacheDesignVariables(designVariables)
+
+        if identicalVariables is False:
+            # Helmholtz Filter
+            u = fd.TrialFunction(self.functionSpace)
+            v = fd.TestFunction(self.functionSpace)
+
+            # DG specific relations
+            n = fd.FacetNormal(self.mesh)
+            h = 2 * fd.CellDiameter(self.mesh)
+            h_avg = (h("+") + h("-")) / 2
+            alpha = 1
+
+            # weak variational form
+            a = (fd.Constant(self.helmholtzFilterRadius) ** 2) * (
+                fd.dot(fd.grad(v), fd.grad(u)) * fd.dx
+                - fd.dot(fd.avg(fd.grad(v)), fd.jump(u, n)) * fd.dS
+                - fd.dot(fd.jump(v, n), fd.avg(fd.grad(u))) * fd.dS
+                + alpha / h_avg * fd.dot(fd.jump(v, n), fd.jump(u, n)) * fd.dS
+            ) + fd.inner(u, v) * fd.dx
+            L = fd.inner(self.rho, v) * fd.dx
+
+            # solve helmholtz equation
+            self.rho_ =fd.Function(self.functionSpace, name="rho_")
+            fd.solve(a == L, self.rho_)
+
+            # projection filter
+            self.rho_hat = (fd.tanh(self.beta * self.eta0) + fd.tanh(self.beta * (self.rho_ - self.eta0))
+            ) / (fd.tanh(self.beta * self.eta0) + fd.tanh(self.beta * (1 - self.eta0)))
+
+            # output rho_hat visualisation
+            self.rho_hatFunction.assign(fd.project(self.rho_hat, self.functionSpace))
+            self.rho_hatFile.write(self.rho_hatFunction)
+
+            # linear elasticity load case
+            u = fd.TrialFunction(self.vectorFunctionSpace)
+            v = fd.TestFunction(self.vectorFunctionSpace)
+
+            # define surface traction
+            x, y = fd.SpatialCoordinate(self.mesh)
+            '''T = fd.conditional(fd.gt(x, 0.3 - (0.3 / 360) - 1e-8),
+                fd.conditional(fd.gt(y, 0.05 - 3 * (0.1 / 120) - 1e-8),
+                    fd.conditional(fd.lt(y, 0.05 + 3 * (0.1 / 120) + 1e-8),
+                        fd.as_vector([0, -1]),
+                        fd.as_vector([0, 0]),),
+                    fd.as_vector([0, 0]),),
+                fd.as_vector([0, 0]),)'''
+            T = fd.as_vector([0, -10])
+            # elasticity parameters
+            self.E = self.E0 + (self.E1 - self.E0) * (self.rho_hat**self.penalisationExponent)
+            lambda_ = (self.E * self.nu) / ((1 + self.nu) * (1 - 2 * self.nu))
+            mu = (self.E) / (2 * (1 + self.nu))
+
+            # linear elastic weak variational form
+            Id = fd.Identity(self.mesh.geometric_dimension())
+            def epsilon(u): return 0.5 * (fd.grad(u) + fd.grad(u).T)
+            def sigma(u): return lambda_ * fd.div(u) * Id + 2 * mu * epsilon(u)
+
+            a = fd.inner(sigma(u), epsilon(v)) * fd.dx
+            L = fd.dot(T, v) * fd.ds(8)
+
+            # solve
+            u =fd.Function(self.vectorFunctionSpace)
+            fd.solve(a == L, u, bcs=self.bcs)
+
+            # output displacement visualisation
+            self.uFunction.assign(u)
+            self.uFile.write(self.uFunction)
+
+            # stress calculation
+            DG0 = fd.FunctionSpace(self.mesh, "DG", 0)
+            sigma_xx = fd.project(sigma(u)[0, 0], DG0)
+            sigma_yy = fd.project(sigma(u)[1, 1], DG0)
+            sigma_xy = fd.project(sigma(u)[0, 1], DG0)
+            von_mises_stress = fd.sqrt(sigma_xx**2 - sigma_xx * sigma_yy + sigma_yy**2 + 3 * sigma_xy**2)
+            von_mises_proj = fd.project(von_mises_stress, DG0)
+            self.stressFunction.assign(von_mises_proj)
+            self.stressFile.write(self.stressFunction)
+            max_stress = np.max(von_mises_proj.vector().get_local())
+
+            # assemble objective function
+            self.j = fd.assemble(fd.inner(T, u) * fd.ds(8))
+
+            # volume fraction constraint
+            volume_fraction = (1 / self.meshVolume) * fd.assemble(self.rho_hat * fd.dx)
+            self.c1 = self.Vlimit - volume_fraction
+
+            stressintegral = fd.assemble( ((von_mises_stress ** self.pnorm) * self.rho_hat * fd.dx) ) ** (1/self.pnorm)
+            self.c2 = self.Slimit - stressintegral
+            print("Stress integral ", stressintegral)
+
+            # compute objective function sensitivities
+            self.djdrho = (fda.compute_gradient(self.j, fda.Control(self.rho)).vector().get_local()) / self.gradientScale
+
+            # compute constraint sensitivities
+            self.dc1drho = (fda.compute_gradient(self.c1, fda.Control(self.rho)).vector().get_local()) / self.gradientScale
+            self.dc2drho = (fda.compute_gradient(self.c2, fda.Control(self.rho)).vector().get_local()) / self.gradientScale
+
+            # assemble constraint vector
+            self.c = np.array([self.c1, self.c2])
+
+            # assemble jacobian vector, np.concatenate((self.dc1drho, self.dc2drho))
+            self.dcdrho = np.concatenate((self.dc1drho, self.dc2drho))
+
+            with open(self.outputFolder2 + "combined_iteration_results.txt", "a") as log_file:
+                log_file.write(f"{self.j:.3e}\t{volume_fraction:.4f}\t{max_stress:.3e}\n")
+
+        else:
+            pass
+
+        return self.j, self.djdrho, self.c, self.dcdrho
---
+++ 2D_TO_pnorm/optimise.py
@@ -0,0 +1,112 @@
+import datetime 
+import numpy as np # type: ignore
+import sys
+from finiteDifferenceValidation import FiniteDifferenceValidation
+from forwardSolve import ForwardSolve
+
+class OptimisationLoop:
+    def __init__(self):
+        # density field bounds
+        self.lowerBound = 0
+        self.upperBound = 1
+
+        # continuation parameters
+        self.beta = None
+        self.penalisationExponent = 3  # fixed at present
+        self.pnorm = None
+
+        # output folder
+        self.outputFolder = ("results/TO" + datetime.datetime.now().strftime("%y_%m-%d-%H-%M-%S") + "/")
+        self.outputFolder2 = ("results/")
+        # finite-difference test flag
+        self.finiteDifferenceValidation = False
+
+        # variable initialisation
+        self.variableInitialisation = False
+        self.rho0 = None
+
+        # scaling parameters
+        self.scalingInitialisation = False
+        self.gradientScaling = 1
+        self.constraintScaling = 1
+        self.jacobianScaling = 1
+
+    def OptimisationSetup(self):
+
+        # initialise forward solve class
+        self.ForwardSolve = ForwardSolve(
+            self.outputFolder,
+            self.outputFolder2,
+            self.beta,
+            self.penalisationExponent,
+            self.variableInitialisation,
+            self.rho0,
+            self.pnorm,)
+
+        # setup forward solve (mesh, function spaces, boundary conditions etc)
+        # mesh: needs changing
+        self.ForwardSolve.Setup()
+
+        # compute initial solution as a function of spatial coordinates
+        self.designVariables0 = self.ForwardSolve.ComputeInitialSolution()
+
+        # perform initial cache of design variables
+        self.ForwardSolve.CacheDesignVariables(None, initialise=True)
+
+        if self.finiteDifferenceValidation is True:
+            FiniteDifferenceValidation(self)
+            sys.exit()
+        else:
+            pass
+
+        # execute initial forward solve (various parameters derived)
+        self.j, self.djdrho, self.c, self.dcdrho = self.ForwardSolve.Solve(self.designVariables0)
+
+        # use user supplied scaling derived from previous continuation iteration
+        if self.scalingInitialisation is True:
+            pass
+        else:
+
+            # gradient scaling (parsed directly to ipopt)
+            self.gradientScaling = 1 / np.average(np.abs(self.djdrho))
+
+            # constraint and jacobian scaling
+            self.constraintScaling = np.zeros(len(self.c))
+            self.jacobianScaling = np.zeros(len(self.c) * self.ForwardSolve.numberOfNodes)
+            for i in range(len(self.c)):
+
+                # constraint scaling
+                self.constraintScaling[i] = 1 / np.average(
+                    np.abs(
+                        self.dcdrho[
+                            i
+                            * self.ForwardSolve.numberOfNodes : (i + 1)
+                            * self.ForwardSolve.numberOfNodes]))
+
+                # jacobian scaling
+                self.jacobianScaling[
+                    i
+                    * self.ForwardSolve.numberOfNodes : (i + 1)
+                    * self.ForwardSolve.numberOfNodes
+                ] = 1 / np.average(
+                    np.abs(
+                        self.dcdrho[
+                            i
+                            * self.ForwardSolve.numberOfNodes : (i + 1)
+                            * self.ForwardSolve.numberOfNodes]))
+
+    def Objective(self, designVariables):
+        self.j, self.djdrho, self.c, self.dcdrho = self.ForwardSolve.Solve(designVariables)
+        return self.j
+
+    def Gradient(self, designVariables):
+        self.j, self.djdrho, self.c, self.dcdrho = self.ForwardSolve.Solve(designVariables)
+        return self.djdrho
+
+    def Constraints(self, designVariables):
+        self.j, self.djdrho, self.c, self.dcdrho = self.ForwardSolve.Solve(designVariables)
+        return self.c
+
+    def Jacobian(self, designVariables):
+        self.j, self.djdrho, self.c, self.dcdrho = self.ForwardSolve.Solve(designVariables)
+        return self.dcdrho
