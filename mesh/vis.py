9931ba0a1b451f84a7b2e72533ed039f9ff04b38
sashalsey <sasha.halsey20@imperial.ac.uk>
2025-02-06 15:07:07 +0000

    extruded corner mesh

--- mesh/vis.py
+++ mesh/vis.py
@@ -1,5 +1,7 @@
 import firedrake as fd # type: ignore
 from firedrake.output import VTKFile # type: ignore
-mesh = fd.Mesh('cylinder.msh')
-vtkfile = VTKFile("mesh.pvd")
+
+m = fd.Mesh('corner3.msh')
+mesh = fd.ExtrudedMesh(m, 5, layer_height = 0.1)
+vtkfile = VTKFile("cornerextruded.pvd")
 vtkfile.write(mesh)
